e751deafd7697ca73f91567099f6788c1dfa9dd8
kidloulou <140658368+kidloulou@users.noreply.github.com>
2024-01-25 10:10:26 +0100

    Add files via upload

---
+++ core/interaction/index.js
@@ -0,0 +1,44 @@
+"use stict"
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.querySelector(".carousel-container");
+    const gButton = document.getElementById("g");
+    const dButton = document.getElementById("d");
+
+    let currentIndex = 0;
+
+    function showImage(index) {
+        const newTransformValue = -index * 100 + "%";
+        container.style.transform = `translateX(${newTransformValue})`;
+    }
+
+    function nextImage() {
+        currentIndex = (currentIndex + 1) % 3;
+        showImage(currentIndex);
+    }
+
+    function prevImage() {
+        currentIndex = (currentIndex - 1 + 3) % 3;
+        showImage(currentIndex);
+    }
+
+    gButton.addEventListener("click", prevImage);
+    dButton.addEventListener("click", nextImage);
+
+    console.log("test");
+    console.log("test2");
+
+
+
+});
+
+
+var titre = document.createElement('h2');
+
+
+titre.textContent = 'Tableau des voitures à vendre';
+
+
+var conteneurTitre = document.getElementById('conteneurTitre');
+
+
+conteneurTitre.appendChild(titre);
